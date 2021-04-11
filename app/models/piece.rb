060ce95cef6058da163167f98dba03f79e577472
Francisca <fvargasserani@gmail.com>
2021-04-10 22:22:43 -0400

    grayscale

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,6 +1,4 @@
 class Piece < ApplicationRecord
     belongs_to :board_game
-    has_many_attached :images do |attachable|
-        attachable.variant :thumb, resize: "100x100"
-    end
+    has_many_attached :images
 end
