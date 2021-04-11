840f55dcbd0355032fe43e0f553e4084048b5d02
Francisca <fvargasserani@gmail.com>
2021-04-11 09:36:16 -0400

    changing models and relations

--- app/models/board_game.rb
+++ app/models/board_game.rb
@@ -1,5 +1,5 @@
 class BoardGame < ApplicationRecord
-    has_many :pieces
-    has_one_attached :image
+    has_many_attached :pieces
+    has_one_attached :box
     has_one_attached :rules_pdf
 end
--- app/models/piece.rb
+++
@@ -1,4 +0,0 @@
-class Piece < ApplicationRecord
-    belongs_to :board_game
-    has_many_attached :images
-end
