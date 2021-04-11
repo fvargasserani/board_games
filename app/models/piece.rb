2610b53100559b025227eb4dc696ddb8207a5c76
Francisca <fvargasserani@gmail.com>
2021-04-10 21:23:54 -0400

    done

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,6 @@
 class Piece < ApplicationRecord
     belongs_to :board_game
-    has_many_attached :images
+    has_many_attached :images do |attachable|
+        attachable.variant :thumb, resize: "100x100"
+    end
 end
