9fb04b6cda758cea0307a06acff095f372a63cc9
Francisca <fvargasserani@gmail.com>
2021-04-15 21:11:58 -0400

    validation

--- app/models/board_game.rb
+++ app/models/board_game.rb
@@ -2,4 +2,5 @@ class BoardGame < ApplicationRecord
     has_many_attached :pieces
     has_one_attached :box
     has_one_attached :rules_pdf
+    validates :rules_pdf, attached: true, content_type: { in: 'application/pdf', message: 'This is not a PDF file' }
 end
